110866dc563923534ebfd08d943315b6ac45a309
fnabilz <fikrizamzak@gmail.com>
2025-05-30 00:35:35 +0800

    project progress 1

--- Java/GroupProjectBank/src/App.java
+++ Java/GroupProjectBank/src/App.java
@@ -1,5 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        ArrayList<User> userList = new ArrayList<>();
+        User currentUser = null;
+
+        User user1 = new User("fnz", "Fikri", "Zamri", "fikri@gmail.com", "12345678", "AccountHolder");
+
+        userList.add(user1);
+
+        System.out.println("-------[Bank System]-------");
+        System.out.println("Hello! Please login to continue... ");
+        while (currentUser == null) {
+            System.out.println("\nID/Email: ");
+            String input = scan.nextLine();
+            System.out.println("Password :");
+            String password = scan.nextLine();
+            currentUser = User.login(userList, input, password);
+        }
+
+        System.out.println("\nHello " + currentUser.getName() + ",");
+        System.out.println("-------[Menu]-------");
+
+        scan.close();
     }
 }
---
+++ Java/GroupProjectBank/src/User.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+
+public class User {
+
+    // properties
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String type;
+
+    // constructor
+    public User(String id, String firstName, String lastName, String email, String password, String type) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.type = type;
+    }
+
+    // methods
+    public static User login(ArrayList<User> userList, String input, String password) {
+        if (!input.isEmpty() && !password.isEmpty()) {
+            for (int i = 0; i < userList.size(); i++) {
+                if (input.contains("@")) {
+                   if (userList.get(i).getEmail().equals(input)) {
+                        if (userList.get(i).getPassword().equals(password)) {
+                            return userList.get(i);
+                        }
+                   }
+                }
+                else {
+                    if (userList.get(i).getId().equals(input)) {
+                        if (userList.get(i).getPassword().equals(password)) {
+                            return userList.get(i);
+                        }
+                   }
+                }
+            }
+        }
+        System.out.println("[Warning] Please enter the correct id/email and password.");
+        return null;
+    } 
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getName() {
+        return this.firstName + " " + this.lastName;
+    }
+
+    private String getType() {
+        return this.type;
+    }
+
+}
